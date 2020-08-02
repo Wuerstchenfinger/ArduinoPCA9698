59ee7568a768ef934390aac6f6ebeb3369be7927
Fabian Sorkalla <fabian.sorkalla@gmail.com>
2020-08-02 17:33:12 +0200

    First basic version...

---
+++ PCA9698.cpp
@@ -0,0 +1,136 @@
+#include "Arduino.h"
+#include "Wire.h"
+#include "PCA9698.h"
+
+PCA9698::PCA9698()
+{
+	Adr = 0x0;
+}
+
+
+void PCA9698::begin(byte adr)
+{
+	Adr = adr;
+	Wire.begin();
+	Wire.setClock(400000);
+}
+
+void PCA9698::pinMode(byte Port, byte Pin, bool Func)
+{
+	if(Port >= 0 && Port <= 4 && Pin >= 0 && Pin <= 7)
+	{
+		byte Reg = 0x18 + Port;
+		byte Data = readReg(Reg);
+		if(Func == OUTPUT)
+		{
+			Data &= ~(1 << Pin);
+		}
+		if(Func == INPUT)
+		{
+			Data |= (1 << Pin);
+		}
+		writeReg(Reg, Data);	
+	}
+	
+}
+void PCA9698::digitalWrite(byte Port, byte Pin, bool Stat)
+{
+	if(Port >= 0 && Port <= 4 && Pin >= 0 && Pin <= 7)
+	{
+		byte Reg = 0x08 + Port;
+		byte Data = readReg(Reg);
+		if(Stat == LOW)
+		{
+			Data &= ~(1 << Pin);
+		}
+		if(Stat == HIGH)
+		{
+			Data |= (1 << Pin);
+		}
+		writeReg(Reg, Data);	
+	}
+}
+
+void PCA9698::setOutputs(byte bank0, byte bank1, byte bank2, byte bank3, byte bank4)
+{
+	byte Reg = 0x88; //AI bit 1 
+	int length = 5;
+	byte Data [] = {bank0, bank1, bank2, bank3, bank4};
+	writeMultiReg(Reg, Data, length);
+}
+
+bool PCA9698::digitalRead(byte Port, byte Pin)
+{
+	if(Port >= 0 && Port <= 4 && Pin >= 0 && Pin <= 7)
+	{
+		byte Reg = 0x00 + Port;
+		byte Data = readReg(Reg);
+		return bool((Data >> Pin) & 0x01);
+	}
+}
+
+void PCA9698::outputEnable(void)
+{
+	byte Reg = 0x2A;
+	byte Data = readReg(Reg);
+	Data &= ~(1 << 0);
+	writeReg(Reg, Data);
+}
+
+void PCA9698::outputDisable(void)
+{
+	byte Reg = 0x2A;
+	byte Data = readReg(Reg);
+	Data |= (1 << 0);
+	writeReg(Reg, Data);
+}
+
+void PCA9698::digitalWriteALL(bool Stat)
+{
+	byte Reg = 0x29;
+	byte Data = 0;
+	byte Data2 = 0;
+	byte Data3 = 0;
+	if(Stat == HIGH)
+	{
+		Data = 0b10011111;
+		Data3 = 0b11111111;
+		Data2 = 0b00011111;
+	}
+	else if(Stat == LOW)
+	{
+		Data = 0b00000000;
+		Data3 = 0b00000000;
+		Data2 = 0b10000000;
+	}
+	writeReg(Reg, Data);
+	for(uint8_t RegOut = 0x08; RegOut <= 0x0C; RegOut++) //refresh Output register
+		writeReg(RegOut, Data3);
+	writeReg(Reg, Data2);
+}
+
+
+void PCA9698::writeReg(byte Reg, byte Data)
+{
+	Wire.beginTransmission(Adr);
+	Wire.write(uint8_t(Reg));
+	Wire.write(uint8_t(Data));
+	Wire.endTransmission();
+}
+
+void PCA9698::writeMultiReg(byte Reg, byte *Data, int length)
+{
+	Wire.beginTransmission(Adr);
+	Wire.write(Reg);
+	Wire.write(Data, length);
+	Wire.endTransmission(Adr);
+}
+
+byte PCA9698::readReg(byte Reg)
+{
+	Wire.beginTransmission(Adr);
+	Wire.write(uint8_t(Reg));
+	Wire.endTransmission();
+	Wire.requestFrom(Adr, 1);
+	return Wire.read();
+}
---
+++ PCA9698.h
@@ -0,0 +1,76 @@
+#ifndef PCA9698_h
+#define PCA9698_h
+
+#include "Arduino.h"
+
+//
+//!@bief Class for the I2C Port Expander PCA9698 from NXP
+class PCA9698{
+public:
+	//
+	//!@brief The Constructor
+	PCA9698(void);
+	//~PCA9698(void);
+	//
+	//!@param Adr I2C Adress of the IC.
+	//!@brief Starts the Communication between the Arduino and the PCA9698.
+	//!@warning The Adresses of the IC can be found in the datasheet. This methode shifts the adress by one to the 
+	//! right because the Wire library wants a 7 bit adress.
+	void begin(byte Adr);
+	//
+	//!@param Port The port of the pin.
+	//!@param Pin The pin.
+	//!@param Func Desired function. OUTPUT or INPUT
+	//!@brief Sets the pin of the port as input or output
+	void pinMode(byte Port, byte Pin, bool Func);
+	//
+	//!@param Port The port of the pin.
+	//!@param Pin The pin.
+	//!@param Stat The logic level of the pin which should be applyed. HIGH or LOW
+	//!@brief Sets the pin of the port to the high or low.
+	void digitalWrite(byte Port, byte Pin, bool Stat);
+	//
+	//!@param Port The port of the pin.
+	//!@param Pin The pin.
+	//!@return The logic level of the pin.
+	//!@brief Reads the pin of the port and returns the logic level.
+	bool digitalRead(byte Port, byte Pin);
+	//
+	//!@brief Enables all Ouputs.
+	void outputEnable(void);
+	//
+	//!@brief Disalbes all Ouputs.
+	void outputDisable(void);
+	//
+	//!@param Stat The logic level which should be applyed. HIGH or LOW
+	//!@brief Sets all Ouputs of to Low or HiGH
+	void digitalWriteALL(bool Stat);
+	
+	//
+	void setOutputs(byte bank0, byte bank1, byte bank2, byte bank3, byte bank4);
+	
+private:
+	//!
+	//!Contains the I2C Adress of the slave.
+	byte Adr = 0;
+	//
+	//!@param Reg Register of the IC which should be read.
+	//!@return Content of the register.
+	//!@brief Retruns one byte which contains the content of the register.
+	byte readReg(byte Reg);
+	//
+	//!@param Reg Register of the IC which should be written to.
+	//!@Data One byte of data.
+	//!@brief Writes one byte of data to the selected register.
+	void writeReg(byte Reg, byte Data);
+	
+	//
+	//!@param Reg Register of the IC which should be written to. 
+	//!@param Data A pointer on the first field of an array.
+	//!@param length The number of bytes which should be written to the PCA9698.
+	//!@brief Writes an array of bytes to the PCA9698.
+	//!@waring The last bit of the Reg needs to be set to HIGH for auto incrementation (AI bit).
+	void writeMultiReg(byte Reg, byte *Data, int length);
+};
+
+#endif
